bdc1377c8f9c3faf84190436c0e767c999f90b4b
mco <martine.collaudcoen@meteoswiss.ch>
2026-07-06 18:55:27 +0200

    correct bug for BbsF computing and data selection

--- compute_exp_D.m
+++ compute_exp_D.m
@@ -1,7 +1,6 @@
 function data_cal=compute_exp_D(data,shortnames,lambda)
 
 
-
 % if scattering data are given, compute the pair-wise expSC and BF
 % if absorption data are given, compute the expAE, pair-wise for 3w CLAP or
 % PSAP & 2wAE
@@ -51,8 +50,8 @@ if ~isempty(namesBs)
             data_cal.BsBFr=data.(Br{1})./data.(r{1});
         end
     elseif length(namesBs)== 6
-        Cs(1,:)=contains(namesBsG,'0_')& ~contains(namesBsG,'dry') & ~contains(namesBsG,'Q');
-        Cs(2,:)=contains(namesBsG,'1_')& ~contains(namesBsG,'dry') & ~contains(namesBsG,'Q');
+        Cs(1,:)=contains(namesBs,'0_')& ~contains(namesBs,'dry') & ~contains(namesBs,'Q');
+        Cs(2,:)=contains(namesBs,'1_')& ~contains(namesBs,'dry') & ~contains(namesBs,'Q');
 
         for i=1:2
             namesBsx=namesBs(Cs(i,:));
@@ -66,33 +65,33 @@ if ~isempty(namesBs)
             data_cal.(N2)=real(-log(data.(b{1})./data.(r{1}))/log(lambda(1)/lambda(3)));
             data_cal.(N3)=real(-log(data.(g{1})./data.(r{1}))/log(lambda(2)/lambda(3)));
         end
-
-        if ~isempty(namesBbs)
-            Cbs(1,:)=contains(namesBbsG,'0_');
-            Cbs(2,:)=contains(namesBbsG,'1_');
-            for i=1:2
-                namesBbsx=namesBbs(Cbs(i,:));
-                Bb=namesBbsx(startsWith(namesBbbsx,'BbsB'));
-                Bg=namesBbsx(startsWith(namesBsx,'BbsG'));
-                Br=namesBbsx(startsWith(namesBbsx,'BbsR'));
-
-                namesBsx=namesBs(Cs(i,:));
-                b=namesBsx(startsWith(namesBsx,'BsB'));
-                g=namesBsx(startsWith(namesBsx,'BsG'));
-                r=namesBsx(startsWith(namesBsx,'BsR'));
-
-                N1=strcat('BbsFb', num2str(i-1));
-                N2=strcat('BbsFbg', num2str(i-1));
-                N3=strcat('BbsFbr', num2str(i-1));
-                data_cal.(N1)=data.(Bb{1})./data.(b{1});
-                data_cal.(N2)=data.(Bg{1})./data.(g{1});
-                data_cal.(N3)=data.(Br{1})./data.(r{1});
-            end
-
-        end
     end
 
+    if ~isempty(namesBbs)
+        Cbs(1,:)=contains(namesBbs,'0_');
+        Cbs(2,:)=contains(namesBbs,'1_');
+        for i=1:2
+            namesBbsx=namesBbs(Cbs(i,:));
+            Bb=namesBbsx(startsWith(namesBbsx,'BbsB'));
+            Bg=namesBbsx(startsWith(namesBbsx,'BbsG'));
+            Br=namesBbsx(startsWith(namesBbsx,'BbsR'));
+
+            namesBsx=namesBs(Cs(i,:));
+            b=namesBsx(startsWith(namesBsx,'BsB'));
+            g=namesBsx(startsWith(namesBsx,'BsG'));
+            r=namesBsx(startsWith(namesBsx,'BsR'));
+
+            N1=strcat('BbsFb', num2str(i-1));
+            N2=strcat('BbsFbg', num2str(i-1));
+            N3=strcat('BbsFbr', num2str(i-1));
+            data_cal.(N1)=data.(Bb{1})./data.(b{1});
+            data_cal.(N2)=data.(Bg{1})./data.(g{1});
+            data_cal.(N3)=data.(Br{1})./data.(r{1});
+        end
+
+    end
 end
+
 
 % absorption
 if ~isempty(namesBa)
@@ -141,26 +140,27 @@ if ~isempty(namesBa)
         end
 
 
-elseif length(namesBa)==7 % 7w AE
-    if length(lambda)~=7
-        error('the nb of wavelengths is different from the number of abs coef')
+    elseif length(namesBa)==7 % 7w AE
+        if length(lambda)~=7
+            error('the nb of wavelengths is different from the number of abs coef')
+        end
+
+        b=namesBa(startsWith(namesBa,'Ba2'));
+        g=namesBa(startsWith(namesBa,'Ba3'));
+        r=namesBa(startsWith(namesBa,'Ba5'));
+        data_cal.expA_bgAE=real(-log(data.(b{1})./data.(g{1}))/log(470/520));
+        data_cal.expA_brAE=real(-log(data.(b{1})./data.(r{1}))/log(470/660));
+        data_cal.expA_grAE=real(-log(data.(g{1})./data.(r{1}))/log(520/660));
+        a1=namesBa(startsWith(namesBa,'Ba1'));
+        a4=namesBa(startsWith(namesBa,'Ba4'));
+        a6=namesBa(startsWith(namesBa,'Ba6'));
+        a7=namesBa(startsWith(namesBa,'Ba7'));
+        all_Abs=[data.(a1{1}) data.(b{1}) data.(g{1}) data.(a4{1}) data.(r{1}) data.(a6{1}) data.(a7{1})];
+        data_cal.expA_fit=Exp_AE_nan_regr(all_Abs);
+    else
+        warning('the number of abs coef does not allow to understand the instrument type')
     end
 
-    b=namesBa(startsWith(namesBa,'Ba2'));
-    g=namesBa(startsWith(namesBa,'Ba3'));
-    r=namesBa(startsWith(namesBa,'Ba5'));
-    data_cal.expA_bgAE=real(-log(data.(b{1})./data.(g{1}))/log(470/520));
-    data_cal.expA_brAE=real(-log(data.(b{1})./data.(r{1}))/log(470/660));
-    data_cal.expA_grAE=real(-log(data.(g{1})./data.(r{1}))/log(520/660));
-    a1=namesBa(startsWith(namesBa,'Ba1'));
-    a4=namesBa(startsWith(namesBa,'Ba4'));
-    a6=namesBa(startsWith(namesBa,'Ba6'));
-    a7=namesBa(startsWith(namesBa,'Ba7'));
-    all_Abs=[data.(a1{1}) data.(b{1}) data.(g{1}) data.(a4{1}) data.(r{1}) data.(a6{1}) data.(a7{1})];
-    data_cal.expA_fit=Exp_AE_nan_regr(all_Abs);
-else
-    warning('the number of abs coef does not allow to understand the instrument type')
-end
 
 
 end
