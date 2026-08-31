ff14b4ce411b9d720c68158f406417ad33bda977
mco <martine.collaudcoen@meteoswiss.ch>
2026-08-31 16:13:58 +0200

    add the possibility to compute trend also if the period< 10y. Small change needed for all3_trend to adapt. To translate

--- all3_trend.m
+++ all3_trend.m
@@ -8,7 +8,7 @@ function [Tresult_MK,Tresult_LMSlog,Tresult_LMSlin]=all3_trend(data,param, inst,
 Tresult_MK=seasonalKendall_main_D(data,param, inst, station, resolution,varargin{:});
 %Tresult_GSMm=bootstrapconfidence_seas_D(data, param,inst, station, 'lin','granu','monthly',varargin{:});
 %Tresult_GSMd=bootstrapconfidence_seas_D(data, param,inst, station, 'lin','granu','daily',varargin{:});
-Tresult_LMSlog=trend_LMS_D(data, param,inst, station, 'log',varargin{1:2},'fig',0);
+Tresult_LMSlog=trend_LMS_D(data, param,inst, station, 'log',varargin{1:end-2},'fig',0);
 %Tresult_LMSlog2=trend_LMS_deseasonFit(data, param,inst, station, 'log',varargin{:});
 
 Tresult_LMSlin=trend_LMS_D(data, param,inst, station, 'lin',varargin{:});
