3bd8f63abae58ac6aea2ca0aa1e6b72501fe685f
mco <martine.collaudcoen@meteoswiss.ch>
2026-08-31 16:08:54 +0200

    add the creation of _dry neph variables to use if RH is often >50% and if there is trend in RH.

--- read_ebas_merged_STN.m
+++ read_ebas_merged_STN.m
@@ -74,13 +74,13 @@ end
 
 %% Transform the structure in Timetable (necessary for matlab)
 % Modify the parameter's name according to the naming convention
-STN_rd_h=Timetable_naming_trend(merged_data);
+STN_rd_h=Timetable_naming_trend_inst(merged_data);
 names_var=fieldnames(STN_rd_h);
 % % Improve station, limit of scattering to 500 Mm-1 for the eastern stations (ACA, GSM, MCN, SHN)
 % and 100 Mm-1 for western (BBE HCG IBB MRN MZW PAZ SCN SIA)
-west=['BBE','BLI','CRG','GWA','GLR','GBN', 'GRE','HGC', 'IBB', 'MRN', 'MZW', 'PAZ', 'RMN','SCN', 'SIA','TMO','UBW'];
-east=['ACA', 'CRO','DSW','GSM','GGW','LBW', 'MCN','NCC', 'SHN'];
-if contains(west,STN_st.name)
+west=["BBE","BLI","CRG","GWA","GLR","GBN", "GRE","HGC", "IBB", "MRN", "MZW", "PAZ", "RMN","SCN", "SIA","TMO","UBW"];
+east=["ACA", "CRO","DSW","GSM","GGW","LBW", "MCN","NCC", "SHN"];
+if contains(west, STN_st.name)
     CW=startsWith(names_var,["Bs","Bbs"]);
     N=names_var(CW);
     for i=1:length(N)
@@ -95,6 +95,23 @@ elseif contains(east,STN_st.name)
         ind=STN_rd_h.(N{i})>500;
         STN_rd_h.(N{i})(ind)=NaN;
     end
+end
+
+% compute dry scat and backscat with hourly data with RH >50% being removed
+names_var=fieldnames(STN_rd_h);
+
+RH=names_var(contains(names_var,'U'));
+%select RH too high
+
+indRH=STN_rd_h.(RH{1})>50 ;
+
+% select variables associated with RH measurements
+
+S1=names_var(startsWith(names_var,["Bs","Bbs"]));
+for j=1:length(S1)
+    S=strcat((S1{j}),'_dry');
+    STN_rd_h.(S)=STN_rd_h.(S1{j});
+    STN_rd_h.(S)(indRH)=NaN;
 end
 
 % compute the daily data
@@ -115,68 +132,3 @@ if sum(ind)>0
 end
 
 
-
-%   STN_rd: data of the station in daily time granularity
-%   STN_st: information on the station (accronym, lat, lon, alt, env) if
-% available in the original file. ATTENTION: I don't know if these
-% info are right, please check thereafter.
-
-% DEPENDENCE: Timetable_naming_trend.m
-
-% Exemple: [SMR_rd, SMR_st]=EbasNC2Timetable("C:\Users\mco\Downloads\merged_Hyytiälä.nc") ;
-
-% take info from Netcdf
-info=ncinfo(filename);
-
-% Create a structure to store imported netCDF data
-merged_data = struct();
-
-% read all the attributes
-for i=1:length(info.Attributes)
-
-    merged_data.Attributes(i).Name = info.Attributes(i).Name;
-    merged_data.Attributes(i).Value = ncreadatt(filename, "/", info.Attributes(i).Name);
-end
-
-%read all the variables
-for i=1:length(info.Variables)
-    merged_data.Variables(i).Name = info.Variables(i).Name;
-    merged_data.Variables(i).Value = ncread(filename, info.Variables(i).Name);
-end
-
-% Transform the structure in Timetable (necessary for matlab)
-% Modify the parameter's name according to the naming convention
-STN_rd=Timetable_naming_trend(merged_data);
-
-% compute the daily data
-STN_rd=retime(STN_rd,'daily', @nanmedian);
-
-%% take information on the station
-STN_st=struct();
-
-% accronym
-ind_gaw_id=contains({merged_data.Attributes.Name},'ebas_station_gaw_id');
-if sum(ind_gaw_id)>0
-STN_st.name=merged_data.Attributes(ind_gaw_id).Value;
-end
-
-%latitute, longitude and altitude
-ind_gaw_id=contains({merged_data.Attributes.Name},'latitude');
-if sum(ind_gaw_id)>0
-STN_st.lat=merged_data.Attributes(ind_gaw_id).Value;
-end
-
-ind_gaw_id=contains({merged_data.Attributes.Name},'longitude');
-if sum(ind_gaw_id)>0
-STN_st.lon=merged_data.Attributes(ind_gaw_id).Value;
-end
-
-ind_gaw_id=contains({merged_data.Attributes.Name},'altitude');
-if sum(ind_gaw_id)>0
-STN_st.alt=merged_data.Attributes(ind_gaw_id).Value;
-end
-% environment and footprint
-ind_gaw_id=contains({merged_data.Attributes.Name},'ebas_station_setting');
-if sum(ind_gaw_id)>0
-STN_st.env=merged_data.Attributes(ind_gaw_id).Value;
-end
